4b62c875eaf4e1eea54cfaf76349f0995300161a
Dennis Burger <dennis.burger@poort80.nl>
2013-08-19 16:02:29 +0200

    Moved to a simple jquery plugin.

--- js/libs/matchhandlebars.jquery.js
+++ js/libs/matchhandlebars.jquery.js
@@ -1,18 +1,39 @@
 /*
 	@author: Dennis Burger
 	@email: dennis@dutchwebworks.nl
-	@prerequisite: handlebars-1.0.rc.1.min.js
-
-	Using handlebar.js and matching the data-handlebar ID to the script tag ID
-	and compiling Handlebar data
+	@prerequisite: jquery.min.js, handlebars-1.0.rc.1.min.js
 */
 
-$.fn.matchHandlebars = function(dataSource) {
-	var handleResultTemplate = this.data('handlebar-template');
-	var handleResultSource = $('#' + handleResultTemplate).html();
+$.fn.matchHandlebars = function() {
+	return this.each(function() {
+		// Get URL's of handlebar template and json data
+		var handleResultTemplate = $(this).data('handlebar-template');
+		var handleResultSource = $(this).data('handlebar-json');
 
-	var handleTemplate = Handlebars.compile(handleResultSource);
-	this.html(handleTemplate(dataSource));
-	
-    return this;
+		// create basic vars
+		var thisObject = $(this);
+		var thisTemplate, thisSource;
+
+		// Ajax load the handlebar template
+		$.ajax({ 
+			async:false, 
+			url: handleResultTemplate,
+			success : function(data){ 
+				thisTemplate = data;
+			}
+		});
+
+		// Ajax load the json data
+		$.ajax({
+			async:false,
+			url: handleResultSource,
+			success : function(data){
+				thisSource = data;
+			}				
+		});
+
+		// Complile the handlebar template with the json data
+		var handleTemplate = Handlebars.compile(thisTemplate);
+		$(thisObject).html(handleTemplate(thisSource));
+	 });
 }
